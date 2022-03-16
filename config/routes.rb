e43e950d8abed95d0340e2e3c520950e54565ec6
davmz <montanezdavid@gmail.com>
2022-03-16 02:29:12 -0500

    Part #2 of ultimate challenge. Restful routes for alphabetized and missing email for the cutomers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   root to: "home#index"
-  resources :admin
+  # get 'customers/index' => 'customers#index'
+  # get 'customers/index' => 'customers#'
+  resources :customers, only: [:index, :show] do
+    collection do
+      get 'alphabetized'
+      get 'missing_email'
+    end
+  end
+
+
+
+  # get "/customers/:alphabetized" => "pages#alphabetized"
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
